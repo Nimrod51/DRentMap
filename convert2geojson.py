df5cc00cbdd1e0cb5a136240e19f5d1f6adc3796
Nimrod51 <gavishnimrod@gmail.com>
2018-10-01 18:11:29 +0200

    change path reading to win/linux

--- convert2geojson.py
+++ convert2geojson.py
@@ -1,13 +1,19 @@
 # -*- coding: utf-8 -*-
 import json, geojson
-import glob
+import os
 from geopy.geocoders import Nominatim
 
-source_dir=".\data\WGGesuchtDE\\"
+script_dir = os.path.dirname(os.path.abspath(__file__))
+rel_path1 = "data"
+rel_path2 = "WGGesuchtDE"
+source_dir = os.path.join(script_dir, rel_path1, rel_path2)
+
 result = []
-for f in glob.glob(source_dir + "*.json"):
-    with open(f, "rb") as infile:
-        result.append(json.load(infile))
+for f in os.listdir(source_dir):
+    if f.endswith(".json"):
+        with open(os.path.join(source_dir,f), "rb") as infile:
+            result.append(json.load(infile))
+
 
 #Create merged JSON file (optional)
 #with open(source_dir + "\\merged_file.json", "wb") as outfile:
@@ -20,7 +26,7 @@ def formatAddress(address):
 
 
 #Iterate over each result and geocode the address
-geolocator = Nominatim(timeout=3)
+geolocator = Nominatim(user_agent="rent-application",timeout=3)
 features=[]
 successCount=0
 errorCount=0
@@ -50,6 +56,6 @@ except:
 #Write result to geojson file
 fc = geojson.FeatureCollection(features)
 variableStr='var locations='.strip('"')
-with open(source_dir + "\\result.geojson", "w") as outfile:
+with open(os.path.join(source_dir,"result.geojson"), "w") as outfile:
     outfile.write(variableStr)
     geojson.dump(fc, outfile)
